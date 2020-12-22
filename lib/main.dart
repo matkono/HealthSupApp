e3902c42df35e919886a23716602051a15217bdd
Leandro Shibahara <leandrohideyuki@hotmail.com>
2020-12-22 15:49:30 -0300

    Adding responsive framework to the UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:HealthSup/features/decision_tree/presentation/bloc/decision_tree_bloc.dart';
 import 'package:HealthSup/features/decision_tree/presentation/pages/medical_appointment.dart';
 import 'package:HealthSup/features/login/presentation/bloc/login_bloc.dart';
@@ -11,6 +9,7 @@ import 'package:HealthSup/features/patient/presentation/pages/patient_homepage.d
 import 'package:HealthSup/features/patient/presentation/widgets/patient_details.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 import 'injection_container.dart' as di;
 
 Future<void> main() async {
@@ -36,6 +35,22 @@ class MyApp extends StatelessWidget {
         ),
       ],
       child: MaterialApp(
+        builder: (context, widget) => ResponsiveWrapper.builder(
+          BouncingScrollWrapper.builder(context, widget),
+          maxWidth: 1200,
+          minWidth: 450,
+          defaultScale: false,
+          breakpoints: [
+            ResponsiveBreakpoint.resize(450, name: MOBILE),
+            ResponsiveBreakpoint.autoScale(800, name: TABLET),
+            ResponsiveBreakpoint.autoScale(1000, name: TABLET),
+            ResponsiveBreakpoint.resize(1200, name: DESKTOP),
+            ResponsiveBreakpoint.autoScale(2460, name: "4K"),
+          ],
+          background: Container(
+            color: Color(0xFFF5F5F5),
+          ),
+        ),
         home: PatientHomePage(),
         debugShowCheckedModeBanner: false,
         routes: {
